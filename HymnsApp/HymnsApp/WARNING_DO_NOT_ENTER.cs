0bd03d8fc040b0cc0bf3eb6f61ceb276b317cf7f
athan isaac <athani@cs.washington.edu>
2020-03-23 23:03:40 -0700

    fixed merge

--- HymnsApp/HymnsApp/WARNING_DO_NOT_ENTER.cs
+++ HymnsApp/HymnsApp/WARNING_DO_NOT_ENTER.cs
@@ -90,6 +90,10 @@ namespace HymnsApp
 
         public List<KeyValuePair<string, string>> StudentsOfGrade(string className)
         {
+            if (className == CurrentClass)
+            {
+                return list;
+            }
             CurrentClass = className;
             list = new List<KeyValuePair<string, string>>();
             StudentsOfGrade(className, list);
