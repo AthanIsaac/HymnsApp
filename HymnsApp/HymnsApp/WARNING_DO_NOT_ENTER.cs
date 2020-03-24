e316d50c0ac6fb16f9e24cb53b8bb872c12e870e
athan isaac <athani@cs.washington.edu>
2020-03-23 18:59:52 -0700

    finished testing the dangerous file

--- HymnsApp/HymnsApp/WARNING_DO_NOT_ENTER.cs
+++ HymnsApp/HymnsApp/WARNING_DO_NOT_ENTER.cs
@@ -9,7 +9,6 @@ namespace HymnsApp
 {
     public class HymnsAttendance2 : IHymnsAttendance
     {
-
         private static readonly string PROJECT_ID = "hymnsapp-47352";
 
         private readonly FirestoreDb db;
@@ -19,6 +18,8 @@ namespace HymnsApp
         public readonly string[] StudentFields = { "studentName", "studentPhone", "grade", "parentName", "parentPhone", "birthday" };
 
         public static IDictionary<string, string> Classes;
+
+        List<KeyValuePair<string, string>> list;
 
         // studentId -> studentSnapshot
         private readonly IDictionary<string, DocumentSnapshot> Students;
@@ -30,6 +31,7 @@ namespace HymnsApp
         public HymnsAttendance2()
         {
             Students = new Dictionary<string, DocumentSnapshot>();
+            Classes = new Dictionary<string, string>();
             SetEnvironmentVariables();
             db = FirestoreDb.Create(PROJECT_ID);
             GetClasses().Wait();
@@ -52,16 +54,16 @@ namespace HymnsApp
         {
             string jsonCred =
                 @"{  
-                    'type': 'service_account', 'project_id': 'hymnsapp-47352',
-                    'private_key_id': 'c77f0a0bf42582ab067216779bf21c7ff44746f8',
-                    'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCsVqhSIp8YRzov\nqdLuSr61fGbPxdHYkogtSaqRzLkZ2c+Z4jK0/Tq+gEyPwU65imVsM6tmoAojoBMa\nNYzj+V6kevUU49bJS0SbT2s9xHsoUeC4Eouh9k89euYSKOMUJY1WJ/WzW+YhiT4j\n2RBadxv7WW6Pj9zwXP4kIG9KSBsj3cAyy1QTxJOp60+FUQ0tblOcXiwZeJ14Vd8o\nvUgxW63+rotpcDHI5y9tK8G+TFw1z6Ktbtvf1+YqMoQrDqd+MXfOY0XRwZp+etkR\npw4BUaWB8v0iyDzCtTTARYeB0YuEWXRxkB7yEOdUFkkwJ7LlqdxWpOtPHsZyyifh\n18E6mjaDAgMBAAECggEAOfJZXmStwegRNH2RovYW8ccGes41JPEAQNoINEX15y9J\nkqBwFFMhMXTKSXviEJjsSVmGRFeDkT79rU5cNTtgC0Ycqg2B5uUcCGzHzlkGYCRH\nvxzdPnQnaLCuM2T0FY4a2+FLyCcaViRjdIJD8WcjTXxgpHzm+RsLiPIu+XcNSR11\nhekVHqCnD1H8nnzub1W7oifSfmOYP9X8QqbTbMSzAjAnZm9vWPYqTUv9T4pReHKp\nX7Nn7FQoC+uqfgtGPTFVCnBB+QnWSz3ieNIpB0Mwhry+sW40fxeP/3dOeF7mgtcR\noh5Q8bM5x1daYnKDRaOnWScniDjISpgkvi5lHPnO4QKBgQDS09HRFSUl1qfqXs+W\n24M2fErRne7p9L29AZ2QxvizzHE9Da2K2Q2Jqgq9pavFuP6uVcPoFY21FtYJRuBs\nNLG5rCyWQ6nEfIhSWaBPXn0K0vzbd4WCWAJax4jP4ru03+BisRucDlWr8017q3wv\nI7UXZ7i84JVrz625od6HymZu5QKBgQDRQ6q1+ZVfO/JaaRQP/cKmoSyIRpXAq+dc\n1heDg5JVDo+J2w252Gl6mS/bHZqU4B6tN4B7zysNho/wq+K3K0yB6XWZvu9Gow4E\nJ6N26s8fkEbg9VKNR6j2y0BvgTCGXpxtVEZgx+TryuhJ/SedUGW5ldutQQoRgGSy\nw+kDxf9RRwKBgFweqeNN9flenehOnS4xpFe+X4LQG4Cmq/FWL17/UdReVGx0+Ytd\n5AhvAFp1dWTjvIS4fO1/3XdvDv7mtVEShUW38ZOG5Tsxnu88skt74e9E2a+bvf6C\nLiU0YpdTXuldmVIGSSYbLVwA565N8+k1FP+xFouRJjkBectO60kyKyfxAoGAXi39\nC28rSbQaC5SVAelsDEAnYaGazh7Pvplf9cFmPz+RXDKpB8YdGp8MyamWI4CGbeC3\nw4DmWG0CJQfGvjcPdLxUQACNgZXqvfX0/JUK6KKvM5lVMN5abc/lzQkwhJjZ/95Y\n2j+8iF5Gkt4LUSKY1aBbQKsv09ab+1Rub1NR6AsCgYEAiaJLHmV5o4be5JlSMYUP\nQAupHQlajaAzBrhPgP1YEBC36/lbtgmw/HjD4CxxVYiHnfECISDk/DjZILyX72tW\nd+XpUN23ILX/kqUNAZKZXJrHp6ivLKHFO4yfok1dBf7Wv3TfPDJgZgRzw278IA0r\n8XcHzVxE6znzDksOV8IOjEw=\n-----END PRIVATE KEY-----\n',
-                    'client_email': 'connecthymnsapp@hymnsapp-47352.iam.gserviceaccount.com',
-                    'client_id': '110366193664730016820',
-                    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-                    'token_uri': 'https://oauth2.googleapis.com/token',
-                    'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-                    'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/connecthymnsapp%40hymnsapp-47352.iam.gserviceaccount.com' 
-                }";
+                'type': 'service_account', 'project_id': 'hymnsapp-47352',
+                'private_key_id': 'c77f0a0bf42582ab067216779bf21c7ff44746f8',
+                'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCsVqhSIp8YRzov\nqdLuSr61fGbPxdHYkogtSaqRzLkZ2c+Z4jK0/Tq+gEyPwU65imVsM6tmoAojoBMa\nNYzj+V6kevUU49bJS0SbT2s9xHsoUeC4Eouh9k89euYSKOMUJY1WJ/WzW+YhiT4j\n2RBadxv7WW6Pj9zwXP4kIG9KSBsj3cAyy1QTxJOp60+FUQ0tblOcXiwZeJ14Vd8o\nvUgxW63+rotpcDHI5y9tK8G+TFw1z6Ktbtvf1+YqMoQrDqd+MXfOY0XRwZp+etkR\npw4BUaWB8v0iyDzCtTTARYeB0YuEWXRxkB7yEOdUFkkwJ7LlqdxWpOtPHsZyyifh\n18E6mjaDAgMBAAECggEAOfJZXmStwegRNH2RovYW8ccGes41JPEAQNoINEX15y9J\nkqBwFFMhMXTKSXviEJjsSVmGRFeDkT79rU5cNTtgC0Ycqg2B5uUcCGzHzlkGYCRH\nvxzdPnQnaLCuM2T0FY4a2+FLyCcaViRjdIJD8WcjTXxgpHzm+RsLiPIu+XcNSR11\nhekVHqCnD1H8nnzub1W7oifSfmOYP9X8QqbTbMSzAjAnZm9vWPYqTUv9T4pReHKp\nX7Nn7FQoC+uqfgtGPTFVCnBB+QnWSz3ieNIpB0Mwhry+sW40fxeP/3dOeF7mgtcR\noh5Q8bM5x1daYnKDRaOnWScniDjISpgkvi5lHPnO4QKBgQDS09HRFSUl1qfqXs+W\n24M2fErRne7p9L29AZ2QxvizzHE9Da2K2Q2Jqgq9pavFuP6uVcPoFY21FtYJRuBs\nNLG5rCyWQ6nEfIhSWaBPXn0K0vzbd4WCWAJax4jP4ru03+BisRucDlWr8017q3wv\nI7UXZ7i84JVrz625od6HymZu5QKBgQDRQ6q1+ZVfO/JaaRQP/cKmoSyIRpXAq+dc\n1heDg5JVDo+J2w252Gl6mS/bHZqU4B6tN4B7zysNho/wq+K3K0yB6XWZvu9Gow4E\nJ6N26s8fkEbg9VKNR6j2y0BvgTCGXpxtVEZgx+TryuhJ/SedUGW5ldutQQoRgGSy\nw+kDxf9RRwKBgFweqeNN9flenehOnS4xpFe+X4LQG4Cmq/FWL17/UdReVGx0+Ytd\n5AhvAFp1dWTjvIS4fO1/3XdvDv7mtVEShUW38ZOG5Tsxnu88skt74e9E2a+bvf6C\nLiU0YpdTXuldmVIGSSYbLVwA565N8+k1FP+xFouRJjkBectO60kyKyfxAoGAXi39\nC28rSbQaC5SVAelsDEAnYaGazh7Pvplf9cFmPz+RXDKpB8YdGp8MyamWI4CGbeC3\nw4DmWG0CJQfGvjcPdLxUQACNgZXqvfX0/JUK6KKvM5lVMN5abc/lzQkwhJjZ/95Y\n2j+8iF5Gkt4LUSKY1aBbQKsv09ab+1Rub1NR6AsCgYEAiaJLHmV5o4be5JlSMYUP\nQAupHQlajaAzBrhPgP1YEBC36/lbtgmw/HjD4CxxVYiHnfECISDk/DjZILyX72tW\nd+XpUN23ILX/kqUNAZKZXJrHp6ivLKHFO4yfok1dBf7Wv3TfPDJgZgRzw278IA0r\n8XcHzVxE6znzDksOV8IOjEw=\n-----END PRIVATE KEY-----\n',
+                'client_email': 'connecthymnsapp@hymnsapp-47352.iam.gserviceaccount.com',
+                'client_id': '110366193664730016820',
+                'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+                'token_uri': 'https://oauth2.googleapis.com/token',
+                'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+                'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/connecthymnsapp%40hymnsapp-47352.iam.gserviceaccount.com' 
+            }";
 
             // connect to database
             var fileName = @"HymnsApp-c77f0a0bf425.json";
@@ -82,7 +84,7 @@ namespace HymnsApp
         public List<KeyValuePair<string, string>> StudentsOfGrade(string className)
         {
             CurrentClass = className;
-            List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>();
+            list = new List<KeyValuePair<string, string>>();
             StudentsOfGrade(className, list).Wait();
             return list;
         }
@@ -135,11 +137,14 @@ namespace HymnsApp
                 }
                 catch
                 {
-                    Dictionary<string, object> update = new Dictionary<string, object>
+                    if (studentIds.Contains(studentId))
+                    {
+                        Dictionary<string, object> update = new Dictionary<string, object>
                     {
                         { "attended", new string[] { date } }
                     };
-                    await dr.SetAsync(update, SetOptions.MergeAll);
+                        await dr.SetAsync(update, SetOptions.MergeAll);
+                    }
                 }
             }
         }
@@ -162,7 +167,8 @@ namespace HymnsApp
         }
         private async Task AddStudentHelper(string studentName, string studentPhone, string grade, string parentName, string parentPhone, DateTime birthday)
         {
-            Dictionary<string, object> student = new Dictionary<string, object> {
+            Dictionary<string, object> student = new Dictionary<string, object> 
+            {
                 { "studentName", studentName },
                 { "studentPhone", studentPhone },
                 { "grade", grade },
@@ -173,8 +179,12 @@ namespace HymnsApp
             // check if there is a student with this name and grade or birthday?
             DocumentReference dr = await db.Collection("students").AddAsync(student);
             string studentId = dr.Id;
-            DocumentReference middle = db.Document("classes/" + CurrentClass);
+            DocumentReference middle = db.Document("classes/" + Classes[CurrentClass]);
             await middle.UpdateAsync("students", FieldValue.ArrayUnion(studentId));
+
+            Students.Add(studentId, await dr.GetSnapshotAsync());
+            list.Add(new KeyValuePair<string, string>(studentId, studentName));
+            list.Sort((a, b) => a.Value.CompareTo(b.Value));
         }
 
         public string GetStudentPhone(string studentId)
@@ -243,16 +253,16 @@ namespace HymnsApp
             };
 
             await dr.SetAsync(update, SetOptions.MergeAll);
+            Students[studentId] = await dr.GetSnapshotAsync();
 
-
-            if(CurrentClass != newClassName)
+            if (CurrentClass != newClassName)
             {
                 //Remove student from current class
-                DocumentReference curClass = db.Document("classes/" + CurrentClass);
+                DocumentReference curClass = db.Document("classes/" + Classes[CurrentClass]);
                 await curClass.UpdateAsync("students", FieldValue.ArrayRemove(studentId));
 
                 //add student from current class
-                DocumentReference newClass = db.Document("classes/" + newClassName);
+                DocumentReference newClass = db.Document("classes/" + Classes[newClassName]);
                 await newClass.UpdateAsync("students", FieldValue.ArrayUnion(studentId));
 
             }
@@ -260,15 +270,24 @@ namespace HymnsApp
 
         public bool AttendedToday(string studentId, DateTime date)
         {
-            DocumentSnapshot s = Students[studentId];
-            string[] attended = s.GetValue<string[]>("attended");
-
             string d = date.ToString("MM/dd/yyyy");
-            foreach (string a in attended)
+
+            DocumentSnapshot s = Students[studentId];
+            try
             {
-                if (a == d) return true;
+                string[] attended = s.GetValue<string[]>("attended");
+
+                foreach (string a in attended)
+                {
+                    if (a == d) return true;
+                }
+                return false;
             }
-            return false;
+            catch
+            {
+                return false;
+            }
+
         }
 
         public string[] WeeklyBirthdays()
