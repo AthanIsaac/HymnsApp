4d0106e041281178e39fffa4f9775a8c5495acd6
Nardin Eshak <nardineshak@Nardins-MacBook-Pro.local>
2020-03-23 17:18:28 -0700

    update colors and splashscreen

--- HymnsApp/HymnsApp.Android/SplashActivity.cs
+++ HymnsApp/HymnsApp.Android/SplashActivity.cs
@@ -3,24 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.V7.App;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
+using HymnsApp.Droid;
 using Xamarin.Forms;
 
+//namespace HymnsApp.Droid
+//{
+//    [Activity(Label = "Hymns App", Icon = "@drawable/HymnsLogoCrop", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+//    class SplashActivity : AppCompatActivity
+//    {
+//        protected override void OnResume()
+//        {
+//            ActivityIndicator activityIndicator = new ActivityIndicator { IsRunning = true };
+//            base.OnResume();
+//            StartActivity(typeof(MainActivity));
+//        }
+//    }
+//}
 namespace HymnsApp.Droid
 {
-    [Activity(Label = "Hymns App", Icon = "@drawable/HymnsLogoCrop", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
-    class SplashActivity : AppCompatActivity
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
     {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
         protected override void OnResume()
         {
-            ActivityIndicator activityIndicator = new ActivityIndicator { IsRunning = true };
             base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+            await Task.Delay(8000); // Simulate a bit of startup work.
+            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
             StartActivity(typeof(MainActivity));
         }
     }
