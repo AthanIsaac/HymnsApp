f8eded2954680db97c194e4af6a341b91aef680d
vgirgis <49889719+vgirgis@users.noreply.github.com>
2020-03-29 20:22:29 -0700

    added curriculum classes

--- HymnsApp/HymnsApp/CurriculumPage.xaml.cs
+++ HymnsApp/HymnsApp/CurriculumPage.xaml.cs
@@ -22,28 +22,81 @@ namespace HymnsApp
 
         }
 
-
-
         public void PrintCurriculum()
         {
 
             var cur = Attendance.GetCurriculum();
 
+            string[] classes = new string[cur.Length];
+
+            for (int i = 0; i < cur.Length; i++) 
+            {
+                classes[i] = parseName(cur[i][0]);
+            }
+
             for (int i = 0; i < cur.Length; i++)
             {
                 
                 StackLayout sl = new StackLayout();
-                for (int j = 0; j < cur[i].Length; j++)
+                for (int j = 1; j < cur[i].Length; j++)
                 {
-                    sl.Children.Add(new Label { Text = (j + 1) + ". " + cur[i][j] });
+                    sl.Children.Add(new Label { Text = (j) + ". " + cur[i][j] });
 
                 }
                 Accordion.CustomControls.Accordion newAccordion = new Accordion.CustomControls.Accordion()
-                { Title = "Class" + i ,AccordionContentView = sl, IndicatorView = new Label (){ Text = "V   " ,FontSize = 23} };
-               
+                { Title = classes[i], AccordionContentView = sl, IndicatorView = new Label (){ Text = "V   ", FontSize = 23} };
 
+                curGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
                 curGrid.Children.Add(newAccordion, 0, i);
             }
+        }
+
+        private string parseName(string c)
+        {
+            if (c.Contains("kindergarten"))
+            {
+                c = "Kindergarten";
+                return c;
+            }
+
+            if (c.Contains("highSchool"))
+            {
+                c = "HighSchool";
+                return c;
+            }
+            if (c.Contains("Grade"))
+            {
+                int index = c.IndexOf("Grade");
+                c = c.Substring(0, index) + " " + c.Substring(index);
+
+                if (c.Contains("&"))
+                {
+                    int ampersand = c.IndexOf("&");
+                    c = c.Substring(0, ampersand) + " & " + c.Substring(ampersand + 1);
+                }
+                return c;
+            }
+
+            else
+            {
+                //really inefficent, find better way
+                for (int j = 0; j < c.Length; j++)
+                {
+
+                    if (char.IsUpper(c[j]))
+                    {
+                        c = c.Substring(0, j) + " " + c.Substring(j);
+                        j++;
+                    }
+
+
+                }
+
+                c = c.Replace("m", "M");
+                c = c.Trim();
+                return c;
+            }
+
         }
     }
 }
